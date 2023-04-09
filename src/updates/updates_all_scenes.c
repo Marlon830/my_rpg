243ea46a0e38b5cfba226363c92bfbbe5b609e2e
Marlon PEGAHI <marlon.pegahi@epitech.eu>
2023-04-09 17:15:35 +0200

    added updated all scenes function so that we can choose which scene update

---
+++ src/updates/updates_all_scenes.c
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2022
+** my_rpg
+** File description:
+** updates_scenes.c
+*/
+
+#include "project.h"
+
+void update_all_scenes(project_t *project)
+{
+    event_all_scenes(project);
+    sfRenderWindow_clear(project->window, sfBlack);
+    switch (project->scene_id) {
+        case 1: update_scene_one(project, project->scene_one); break;
+        default: break;
+    }
+    sfRenderWindow_display(project->window);
+}
