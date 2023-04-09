42d7c510018844305694417b061702e947f86d20
Marlon PEGAHI <marlon.pegahi@epitech.eu>
2023-04-09 17:13:37 +0200

    fixed name from graphic to scene now that each graphics struct is a scene, and created the window in the project struct instead of graphic

--- src/graphic/graphic_init.c
+++ src/graphic/graphic_init.c
@@ -5,39 +5,41 @@
 ** graphic_init.c
 */
 
-#include "../../include/graphic.h"
+#include "project.h"
 
-void next_graphic_init(graphic_t *graphic)
+void next_graphic_init(project_t *project, graphic_t *scene)
 {
-    graphic->player_col->width = 16;
-    graphic->player_col->height = 16;
-    sfView_setSize(graphic->camera, (sfVector2f){256,144});
-    sfView_setCenter(graphic->camera, graphic->player_pos);
-    sfRenderWindow_setView(graphic->window, graphic->camera);
-    push_back(&graphic->images, "map", create_image(0,
+    scene->player_col->width = 16;
+    scene->player_col->height = 16;
+    sfView_setSize(scene->camera, (sfVector2f){256,144});
+    sfView_setCenter(scene->camera, scene->player_pos);
+    sfRenderWindow_setView(project->window, scene->camera);
+    push_back(&scene->images, "map", create_image(0,
     0, "spawn.png", (sfIntRect){0,0, 480, 480}), IMAGE);
-    push_back(&graphic->images, "player", create_image(graphic->player_pos.x,
-    graphic->player_pos.y, "player.png", (sfIntRect){0,0, 16, 16}), IMAGE);
-    colliders_init("./assets/res.coll", graphic);
+    push_back(&scene->images, "player", create_image(scene->player_pos.x,
+    scene->player_pos.y, "player.png", (sfIntRect){0,0, 16, 16}), IMAGE);
+    colliders_init("./assets/res.coll", scene);
+    scene->player_speed = 500;
+    scene->movement = (sfVector2f) {0, 0};
 }
 
-graphic_t *graphic_init(void)
+graphic_t *graphic_init(project_t *project)
 {
-    graphic_t *graphic = malloc(sizeof(graphic_t));
+    graphic_t *scene = malloc(sizeof(graphic_t));
     sfVideoMode mode = (sfVideoMode){1920, 1080, 32};
 
-    graphic->window = sfRenderWindow_create(mode, "Quoi ? Feur",
+    project->window = sfRenderWindow_create(mode, "Quoi ? Feur",
     sfClose | sfFullscreen, NULL);
-    sfRenderWindow_setFramerateLimit(graphic->window, 60);
-    sfRenderWindow_setKeyRepeatEnabled(graphic->window, sfFalse);
-    graphic->camera = sfView_create();
-    graphic->player_pos = (sfVector2f){160, 160};
-    graphic->images = malloc(sizeof(list_t));
-    graphic->images = NULL;
-    graphic->colliders = NULL;
-    graphic->player_col = malloc(sizeof(sfFloatRect));
-    graphic->player_col->left = graphic->player_pos.x;
-    graphic->player_col->top = graphic->player_pos.y;
-    next_graphic_init(graphic);
-    return graphic;
+    sfRenderWindow_setFramerateLimit(project->window, 60);
+    sfRenderWindow_setKeyRepeatEnabled(project->window, sfFalse);
+    scene->camera = sfView_create();
+    scene->player_pos = (sfVector2f){160, 160};
+    scene->images = malloc(sizeof(list_t));
+    scene->images = NULL;
+    scene->colliders = NULL;
+    scene->player_col = malloc(sizeof(sfFloatRect));
+    scene->player_col->left = scene->player_pos.x;
+    scene->player_col->top = scene->player_pos.y;
+    next_graphic_init(project, scene);
+    return scene;
 }
