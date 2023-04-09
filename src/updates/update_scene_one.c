b1477e8f0d9ac06216ca6d1e7909ff92292a19e0
Marlon PEGAHI <marlon.pegahi@epitech.eu>
2023-04-09 17:14:58 +0200

    moved the update function so that it updated only the first scene

---
+++ src/updates/update_scene_one.c
@@ -0,0 +1,31 @@
+/*
+** EPITECH PROJECT, 2022
+** my_rpg
+** File description:
+** update_scene_one.c
+*/
+
+#include "project.h"
+
+void update_scene_one(project_t *project, graphic_t *scene)
+{
+    int movement_x = 0, movement_y = 0;
+    bool moved = false;
+
+    if (sfKeyboard_isKeyPressed(sfKeyZ))
+        movement_y -= scene->player_speed;
+    if (sfKeyboard_isKeyPressed(sfKeyS))
+        movement_y += scene->player_speed;
+    if (sfKeyboard_isKeyPressed(sfKeyQ))
+        movement_x -= scene->player_speed;
+    if (sfKeyboard_isKeyPressed(sfKeyD))
+        movement_x += scene->player_speed;
+    moved = movement_x != 0 || movement_y != 0;
+    if (moved) {
+        project->dt = sfClock_getElapsedTime(project->clock).microseconds;
+        scene->movement.x = movement_x / project->dt;
+        scene->movement.y = movement_y / project->dt;
+        move_camera(project, project->scene_one);
+    }
+    graphic_draw(project, scene);
+}
