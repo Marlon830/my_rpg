0227255622074e5a6f90544d92d9ab06e4d2e1df
Marlon PEGAHI <marlon.pegahi@epitech.eu>
2023-04-09 17:10:48 +0200

    updated function name from cat to my_strcat

--- src/graphic/graphic_create.c
+++ src/graphic/graphic_create.c
@@ -13,7 +13,8 @@ image_t *create_image(int x, int y, char *path, sfIntRect rect)
 {
     image_t *image = malloc(sizeof(image_t));
     image->pos = (sfVector2f){(float)x, (float)y};
-    image->texture = sfTexture_createFromFile(cat("./assets/", path), &rect);
+    image->texture = sfTexture_createFromFile(my_strcat("./assets/", path),
+    &rect);
     image->sprite = sfSprite_create();
     sfSprite_setTexture(image->sprite, image->texture, sfFalse);
     sfSprite_setPosition(image->sprite, image->pos);
