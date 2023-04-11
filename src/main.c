eaa1997a86bad9c1e35d16e52bb74295f9f28e67
Alexandre Bret <alexandre.bret@epitech.eu>
2023-04-11 18:01:58 +0200

    deleted one line

--- src/main.c
+++ src/main.c
@@ -40,7 +40,6 @@ project_t *init_project(void)
     project_t *project = malloc(sizeof(project_t));
     sfVideoMode mode = (sfVideoMode){1920, 1080, 32};
 
-    project->scene_one = graphic_init(project);
     project->inventory = create_inventory();
     project->window = sfRenderWindow_create(mode, "Quoi ? Feur",
     sfClose | sfFullscreen, NULL);
