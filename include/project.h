e49a1a4e886c0f71b4126370e1fb99ca26217777
Marlon PEGAHI <marlon.pegahi@epitech.eu>
2023-04-12 14:15:33 +0200

    updated project h header file to add function for the dialogue system and a structure for the actual dialogue

--- include/project.h
+++ include/project.h
@@ -13,16 +13,31 @@
 
     #define WINDOW project->window
 
+typedef struct actual_dialogue_s {
+    char *dialogue;
+    int pos;
+    sfText *text;
+    sfRectangleShape *rect;
+    int is_displayed;
+    image_t *face;
+} act_dial_t;
+
 typedef struct project_s {
     sfRenderWindow *window;
     scene_t *scene;
     sfClock *clock;
     list_t *all_dialogues;
+    act_dial_t *actual_dial;
     float dt;
     player_t *player;
     int scene_id;
 } project_t;
 
 void update_scene(project_t *project);
+
+list_t *create_all_dialogues(project_t *project, char *path_to_all_dialogue);
+char *get_dialogue_by_id(project_t *project, list_t *dialogue, char *id);
+char *get_dialogue_image(char *dialogue);
+void display_dialogue(project_t *project);
 
 #endif
