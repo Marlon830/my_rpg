4167ca7d1066ca3319344dd04d6330cd6c40fc43
Alexandre Bret <alexandre.bret@epitech.eu>
2023-04-12 14:35:51 +0200

    added 4 events to test the inventory

--- src/scene/scene_event.c
+++ src/scene/scene_event.c
@@ -16,5 +16,15 @@ void scene_event(project_t *project)
             sfRenderWindow_close(project->window);
         if (event.type == sfEvtKeyPressed && event.key.code == sfKeyTab)
             switch_state_inventory(project->inventory);
+        if (event.type == sfEvtKeyPressed && event.key.code == sfKeyA)
+            add_elem(project->inventory->bag, "assets/object/apple.png", 1);
+        if (event.type == sfEvtKeyPressed && event.key.code == sfKeyB)
+            add_elem(project->inventory->bag, "assets/object/strawberry.png",
+            1);
+        if (event.type == sfEvtKeyPressed && event.key.code == sfKeyC)
+            delete_elem(project->inventory->bag, "assets/object/apple.png", 1);
+        if (event.type == sfEvtKeyPressed && event.key.code == sfKeyE)
+            delete_elem(project->inventory->bag, "assets/object/strawberry.png",
+            1);
     }
 }
