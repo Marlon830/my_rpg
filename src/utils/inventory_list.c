b0bf3d2e82654b6486d2744426e1f20abc680a43
Alexandre Bret <alexandre.bret@epitech.eu>
2023-04-12 14:37:00 +0200

    added functions to add an elem in the list and to get the box with the name

--- src/utils/inventory_list.c
+++ src/utils/inventory_list.c
@@ -7,6 +7,52 @@
 
 #include "project.h"
 
+box_t *get_box_with_name(list_box_t *list, char *name)
+{
+    list_box_t *temp = list;
+
+    while (temp != NULL) {
+        if (temp->box->name != NULL && !my_strcmp(temp->box->name, name))
+            return temp->box;
+        temp = temp->next;
+    }
+    return NULL;
+}
+
+void add_new_elem_in_box(box_t *box, char *name, int quantity)
+{
+    box->sprite = sfSprite_create();
+    box->sprite_texture = sfTexture_createFromFile(name, NULL);
+    sfSprite_setTexture(box->sprite, box->sprite_texture, sfTrue);
+    box->pos_sprite = sfRectangleShape_getPosition(box->shape);
+    sfSprite_setPosition(box->sprite, box->pos_sprite);
+    box->name = malloc(my_strlen(name) + 1);
+    my_strcpy(box->name, name);
+    box->quantity += quantity;
+    sfText_setString(box->text, int_to_string(box->quantity));
+    sfSprite_setScale(box->sprite, (sfVector2f) {0.875, 0.875});
+}
+
+bool add_elem(list_box_t *list, char *name, int quantity)
+{
+    box_t *box = get_box_with_name(list, name);
+    list_box_t *temp = list;
+
+    if (box != NULL) {
+        box->quantity += quantity;
+        sfText_setString(box->text, int_to_string(box->quantity));
+        return true;
+    }
+    while (temp != NULL) {
+        if (temp->box->sprite == NULL) {
+            add_new_elem_in_box(temp->box, name, quantity);
+            return true;
+        }
+        temp = temp->next;
+    }
+    return false;
+}
+
 list_box_t *init_list_box(void)
 {
     list_box_t *list = malloc(sizeof(*list));
