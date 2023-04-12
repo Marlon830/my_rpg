2614831feab922cd3e75180a77adf5369acf2593
Alexandre Bret <alexandre.bret@epitech.eu>
2023-04-12 16:42:14 +0200

    added inventory event function to manage event with inventory

--- src/scene/scene_event.c
+++ src/scene/scene_event.c
@@ -7,6 +7,28 @@
 
 #include "../../include/project.h"
 
+void inventory_event(project_t *project, sfEvent event)
+{
+    if (event.type == sfEvtKeyPressed && event.key.code == sfKeyA)
+        add_elem(project->inventory->bag, "assets/object/apple.png", 1);
+    if (event.type == sfEvtKeyPressed && event.key.code == sfKeyB)
+        add_elem(project->inventory->bag, "assets/object/strawberry.png",
+        1);
+    if (event.type == sfEvtKeyPressed && event.key.code == sfKeyC)
+        delete_elem(project->inventory->bag, "assets/object/apple.png", 1);
+    if (event.type == sfEvtKeyPressed && event.key.code == sfKeyE)
+        delete_elem(project->inventory->bag, "assets/object/strawberry.png",
+        1);
+    if (project->inventory->is_active && event.type == sfEvtMouseButtonPressed
+    && event.mouseButton.button == sfMouseLeft)
+        select_box(project, (sfVector2f) {event.mouseButton.x,
+        event.mouseButton.y});
+    if (project->inventory->is_active && event.type == sfEvtMouseButtonReleased
+    && event.mouseButton.button == sfMouseLeft)
+        unselect_box(project, (sfVector2f) {event.mouseButton.x,
+        event.mouseButton.y});
+}
+
 void scene_event(project_t *project)
 {
     sfEvent event;
@@ -16,15 +38,6 @@ void scene_event(project_t *project)
             sfRenderWindow_close(project->window);
         if (event.type == sfEvtKeyPressed && event.key.code == sfKeyTab)
             switch_state_inventory(project->inventory);
-        if (event.type == sfEvtKeyPressed && event.key.code == sfKeyA)
-            add_elem(project->inventory->bag, "assets/object/apple.png", 1);
-        if (event.type == sfEvtKeyPressed && event.key.code == sfKeyB)
-            add_elem(project->inventory->bag, "assets/object/strawberry.png",
-            1);
-        if (event.type == sfEvtKeyPressed && event.key.code == sfKeyC)
-            delete_elem(project->inventory->bag, "assets/object/apple.png", 1);
-        if (event.type == sfEvtKeyPressed && event.key.code == sfKeyE)
-            delete_elem(project->inventory->bag, "assets/object/strawberry.png",
-            1);
+        inventory_event(project, event);
     }
 }
