b6c6a4d79676599b8592fd532f362bb29580e7ac
Alexandre Bret <alexandre.bret@epitech.eu>
2023-04-13 16:30:10 +0200

    added animated inventory sprite function to animate a sprite in the inventory

--- src/updates/update_inventory2.c
+++ src/updates/update_inventory2.c
@@ -24,6 +24,25 @@ void update_selected_box(project_t *project)
     sfRenderWindow_drawSprite(WINDOW, box->sprite, NULL);
 }
 
+void animate_inventory_sprite(box_t *box)
+{
+    sfIntRect rect;
+
+    box->time_box = sfClock_getElapsedTime(box->clock_box);
+    box->seconds_box = box->time_box.microseconds / 1000000.0;
+    if (box->seconds_box <= 0.15)
+        return;
+    if (box->pos_animation == box->max_animation)
+        box->pos_animation = 0;
+    else
+        box->pos_animation++;
+    rect = (sfIntRect) {box->pos_animation * 16, 0, 16, 16};
+    sfTexture_destroy(box->sprite_texture);
+    box->sprite_texture = sfTexture_createFromFile("assets/player.png", &rect);
+    sfSprite_setTexture(box->sprite, box->sprite_texture, sfTrue);
+    sfClock_restart(box->clock_box);
+}
+
 void update_inventory_character(project_t *project, box_t *box)
 {
     sfRectangleShape_setPosition(box->shape,
@@ -35,6 +54,7 @@ void update_inventory_character(project_t *project, box_t *box)
     sfSprite_setPosition(box->sprite, (sfVector2f)
     {project->player->pos.x - 72 + box->more_x,
     project->player->pos.y - 64 + box->more_y + 16});
+    animate_inventory_sprite(project->inventory->character);
     sfRenderWindow_drawSprite(WINDOW, box->sprite, NULL);
 }
 
