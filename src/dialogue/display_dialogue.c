01899bdd5758d5b4a375407c672e161da4a69540
Rodzpm <allan.leherpeux@epitech.eu>
2023-04-13 15:05:31 +0200

    [UPDATE] new create text

--- src/dialogue/display_dialogue.c
+++ src/dialogue/display_dialogue.c
@@ -12,7 +12,8 @@ int display_dialogue_bis(char *dialogue, int act_pos, project_t *project)
     int i = act_pos;
 
     if (dialogue[i] == '#') {
-        project->actual_dial->text = create_text();
+        project->actual_dial->text = create_text((sfVector2f){160, 160},
+        (sfVector2f){0.2, 0.2}, sfWhite);
         project->actual_dial->pos = 0;
         project->actual_dial->is_displayed = 0;
         return -84;
