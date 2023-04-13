ec3fec79e5e235de332ffb3dca5fc99c17bea3b2
Alexandre Bret <alexandre.bret@epitech.eu>
2023-04-13 15:29:48 +0200

    added functions update inventory character and description

--- src/updates/update_inventory2.c
+++ src/updates/update_inventory2.c
@@ -23,3 +23,27 @@ void update_selected_box(project_t *project)
         return;
     sfRenderWindow_drawSprite(WINDOW, box->sprite, NULL);
 }
+
+void update_inventory_character(project_t *project, box_t *box)
+{
+    sfRectangleShape_setPosition(box->shape,
+    (sfVector2f) {project->player->pos.x - 72 +
+    box->more_x, project->player->pos.y - 64 +
+    box->more_y});
+    sfRenderWindow_drawRectangleShape(WINDOW,
+    box->shape, NULL);
+    sfSprite_setPosition(box->sprite, (sfVector2f)
+    {project->player->pos.x - 72 + box->more_x,
+    project->player->pos.y - 64 + box->more_y + 16});
+    sfRenderWindow_drawSprite(WINDOW, box->sprite, NULL);
+}
+
+void update_inventory_description(project_t *project, box_t *box)
+{
+    sfRectangleShape_setPosition(box->shape,
+    (sfVector2f) {project->player->pos.x - 72 +
+    box->more_x, project->player->pos.y - 64 +
+    box->more_y});
+    sfRenderWindow_drawRectangleShape(WINDOW,
+    box->shape, NULL);
+}
