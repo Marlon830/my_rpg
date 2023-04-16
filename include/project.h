112104fd799b43840a087f478e46b6c4fbc03784
Marlon PEGAHI <marlon.pegahi@epitech.eu>
2023-04-16 10:42:53 +0200

    added quests struct and functions in project header file

--- include/project.h
+++ include/project.h
@@ -40,6 +40,16 @@ typedef struct all_pnjs_s {
     char *name;
 } all_pnjs_t;
 
+typedef struct quest_s {
+    int is_act_disp;
+    list_t *actual_quests_list;
+    list_t *finished_quests_list;
+    sfRectangleShape *actual_quests_rect;
+    sfRectangleShape *finished_quests_rect;
+    sfText *actual_quests_text;
+    sfText *finished_quests_text;
+} quest_t;
+
 typedef struct project_s {
     sfRenderWindow *window;
     inventory_t *inventory;
@@ -54,6 +64,7 @@ typedef struct project_s {
     list_t *scenes;
     main_menu_t *main_menu;
     sfEvent event;
+    quest_t *quests;
 } project_t;
 
 void update_scene(project_t *project);
@@ -70,5 +81,10 @@ void set_pnj_dialogue(list_t *all_pnj, char *pnj_id, char *dialogue_id);
 void change_state_with_dialogue(project_t *project);
 void change_state_with_scene(project_t *project);
 void set_all_pnj_dialogues(project_t *project);
+
+void add_quest(char *quest_to_add, quest_t *quests, char *quest_id);
+void update_quest(quest_t *quests);
+quest_t *init_quests(void);
+void finish_quest(char *quest_id, quest_t *quests);
 
 #endif
