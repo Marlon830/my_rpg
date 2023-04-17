4e0466460ce7033e5ad8e3ba5d8575efe5e679ef
Marlon PEGAHI <marlon.pegahi@epitech.eu>
2023-04-17 17:23:28 +0200

    updated quest draw to draw the rectangle texture

--- src/quests/quest_draw.c
+++ src/quests/quest_draw.c
@@ -9,14 +9,24 @@
 
 void quest_draw(project_t *project)
 {
-    sfVector2f text_pos = {project->player->pos.x - 125,
+    sfVector2f rect_pos = {project->player->pos.x - 125,
     project->player->pos.y - 65};
+    sfVector2f text_pos = {rect_pos.x + 3, rect_pos.y + 2.5};
     sfText_setPosition(project->quests->actual_quests_text, text_pos);
     sfText_setPosition(project->quests->finished_quests_text, text_pos);
-    if (project->quests->is_act_disp == 1)
+    sfRectangleShape_setPosition(project->quests->actual_quests_rect,
+    rect_pos);
+    sfRectangleShape_setPosition(project->quests->finished_quests_rect,
+    rect_pos);
+    if (project->quests->is_act_disp == 1) {
+        sfRenderWindow_drawRectangleShape(WINDOW,
+        project->quests->actual_quests_rect, NULL);
         sfRenderWindow_drawText(project->window,
         project->quests->actual_quests_text, NULL);
-    else if (project->quests->is_act_disp == 0)
+    } else if (project->quests->is_act_disp == 0) {
+        sfRenderWindow_drawRectangleShape(WINDOW,
+        project->quests->finished_quests_rect, NULL);
         sfRenderWindow_drawText(project->window,
         project->quests->finished_quests_text, NULL);
+    }
 }
