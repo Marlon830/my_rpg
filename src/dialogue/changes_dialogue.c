f862a278369c0c54787bc26d7644c6f188df0f3e
Marlon PEGAHI <marlon.pegahi@epitech.eu>
2023-04-17 14:49:10 +0200

    added first quest

--- src/dialogue/changes_dialogue.c
+++ src/dialogue/changes_dialogue.c
@@ -9,6 +9,10 @@
 
 void change_state_with_dialogue(project_t *project)
 {
+    if (project->player->player_progress_state == 2) {
+        finish_quest("QUETE1", project->quests);
+        project->status = FIGHT;
+    }
     if (project->player->player_progress_state == 0)
         project->player->player_progress_state = 1;
 }
@@ -25,11 +29,12 @@ void set_all_pnj_dialogues(project_t *project)
 {
     if (project->player->player_progress_state == 0)
         set_pnj_dialogue(project->scene->pnj, "/theodore.png", "Theoronfle");
-    if (project->player->player_progress_state == 1)
+    if (project->player->player_progress_state == 1) {
         set_pnj_dialogue(project->scene->pnj, "/theodore.png", "Theoronfle2");
+        add_quest("Trouver la batterie de theodore", project->quests, "QUETE1");
+    }
     if (project->player->player_progress_state == 2)
         set_pnj_dialogue(project->scene->pnj, "/theodore.png", "Theoronfle3");
     if (project->player->player_progress_state == 3)
         set_pnj_dialogue(project->scene->pnj, "/theodore.png", "Theoronfle4");
-
 }
