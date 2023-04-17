8e779e97bc77f9a78a31e67214a2f45d5b4194dc
Rodzpm <allan.leherpeux@epitech.eu>
2023-04-17 13:00:57 +0200

    [UPDATE] new menu with settings, new game and load game buttons

--- include/menu.h
+++ include/menu.h
@@ -16,12 +16,22 @@
 typedef struct project_s project_t;
 typedef struct image_s image_t;
 
+enum main_menu_state {
+    NAUNE,
+    PLAY,
+    SETTINGS
+};
+
 typedef struct main_menu_s {
     image_t *background;
     button_t *play;
+    button_t *settings;
     button_t *quit;
+    button_t *new_game;
+    button_t *load_game;
     sfText *test;
     sfView *camera;
+    enum main_menu_state state;
 } main_menu_t;
 
 void menu_event(project_t *project);
@@ -29,5 +39,7 @@ main_menu_t *init_main_menu(project_t *project);
 void draw_main_menu(project_t *project);
 void play_button(project_t *project);
 void quit_button(project_t *project);
+void display_play(project_t *project);
+void display_settings(project_t *project);
 
 #endif
