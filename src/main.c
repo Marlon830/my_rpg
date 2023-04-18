078be15ac198d183e434ab6bc6d32b2ecfd1ec9c
Rodzpm <allan.leherpeux@epitech.eu>
2023-04-18 11:12:13 +0200

    [UPDATE] chnage handle end of fight

--- src/main.c
+++ src/main.c
@@ -26,7 +26,7 @@ void scene_management(project_t *project)
     if (project->status == FIGHT) {
         battle_scene_event(project);
         update_battle_scene(project->battle_scene, 0, project->window);
-        handle_end_of_fight(project, 0);
+        handle_end_of_fight(project);
     }
 }
 
