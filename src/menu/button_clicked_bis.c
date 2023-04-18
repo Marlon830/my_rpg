744a514c6644c4111540bdee99bb34df6971a1ce
Marlon PEGAHI <marlon.pegahi@epitech.eu>
2023-04-18 18:01:34 +0200

    added function for the settings buttons

---
+++ src/menu/button_clicked_bis.c
@@ -0,0 +1,66 @@
+/*
+** EPITECH PROJECT, 2022
+** my_rpg
+** File description:
+** button_clicked_bis.c
+*/
+
+#include "project.h"
+
+void set_win_fullscreen(project_t *project)
+{
+    sfRenderWindow_close(project->window);
+    project->window = sfRenderWindow_create(project->mode, "Quoi ? Feur",
+    sfClose | sfFullscreen, NULL);
+    project->is_fullscreen = 1;
+    sfRenderWindow_setFramerateLimit(project->window, 60);
+    sfRenderWindow_setKeyRepeatEnabled(project->window, sfFalse);
+}
+
+void set_win_windowed(project_t *project)
+{
+    sfRenderWindow_close(project->window);
+    project->window = sfRenderWindow_create(project->mode, "Quoi ? Feur",
+    sfClose, NULL);
+    project->is_fullscreen = 0;
+    sfRenderWindow_setFramerateLimit(project->window, 60);
+    sfRenderWindow_setKeyRepeatEnabled(project->window, sfFalse);
+}
+
+void set_first_resolution(project_t *project)
+{
+    sfView *view = sfView_create();
+
+    sfRenderWindow_close(project->window);
+    project->mode = (sfVideoMode) {1920, 1080, 32};
+    if (project->is_fullscreen == 1)
+        project->window = sfRenderWindow_create(project->mode, "Quoi ? Feur",
+        sfClose | sfFullscreen, NULL);
+    else
+        project->window = sfRenderWindow_create(project->mode, "Quoi ? Feur",
+        sfClose, NULL);
+    sfRenderWindow_setSize(WINDOW, (sfVector2u) {1920, 1080});
+    sfView_reset(view, (sfFloatRect) {0, 0, 1920, 1080});
+    sfRenderWindow_setView(WINDOW, view);
+    sfRenderWindow_setFramerateLimit(project->window, 60);
+    sfRenderWindow_setKeyRepeatEnabled(project->window, sfFalse);
+}
+
+void set_second_resolution(project_t *project)
+{
+    sfView *view = sfView_create();
+
+    sfRenderWindow_close(project->window);
+    project->mode = (sfVideoMode) {1920, 1080, 32};
+    if (project->is_fullscreen == 1)
+        project->window = sfRenderWindow_create(project->mode, "Quoi ? Feur",
+        sfClose | sfFullscreen, NULL);
+    else
+        project->window = sfRenderWindow_create(project->mode, "Quoi ? Feur",
+        sfClose, NULL);
+    sfRenderWindow_setSize(WINDOW, (sfVector2u) {1280, 720});
+    sfView_reset(view, (sfFloatRect) {0, 0, 1920, 1080});
+    sfRenderWindow_setView(WINDOW, view);
+    sfRenderWindow_setFramerateLimit(project->window, 60);
+    sfRenderWindow_setKeyRepeatEnabled(project->window, sfFalse);
+}
