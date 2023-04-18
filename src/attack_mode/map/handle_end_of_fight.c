7596c7cc22a0de027daab6006ea8f53bdf94fd50
Allan Leherpeux <allan.leherpeux@epitech.eu>
2023-04-18 17:54:24 +0200

    [UPDATE] fight win condition

--- src/attack_mode/map/handle_end_of_fight.c
+++ src/attack_mode/map/handle_end_of_fight.c
@@ -10,10 +10,12 @@
 
 void handle_end_of_fight(project_t *project)
 {
+    project->fight_win = project->battle_scene->win;
     if (project->battle_scene->win) {
         destroy_battle_scene(project->battle_scene);
         project->battle_scene = create_standard_battle_scene();
         project->status = GAME;
         project->scene = load_scene(project, project->scene_id);
+        set_state_with_end_of_fight(project, project->battle_scene->win);
     }
 }
