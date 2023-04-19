c76e6f9d9d08aeba0217c0be348d51d814a029b5
Alexandre Bret <alexandre.bret@epitech.eu>
2023-04-19 11:54:32 +0200

    added ring equipment management

--- src/updates/update_inventory5.c
+++ src/updates/update_inventory5.c
@@ -7,21 +7,33 @@
 
 #include "project.h"
 
-void update_equipment_inventory_sprite3(project_t *project, box_t *character,
+void update_equipment_inventory_sprite4(project_t *project, box_t *character,
 equipment_t *equipment, sfVector2f pos)
 {
-    if (equipment->sword != NULL) {
-        sfSprite_setPosition(equipment->sword, pos);
-        sfSprite_setTextureRect(equipment->sword, (sfIntRect)
-        {character->pos_animation * 32, 0, 32, 32});
-        sfRenderWindow_drawSprite(WINDOW, equipment->sword, NULL);
-    }
     if (equipment->shield != NULL) {
         sfSprite_setPosition(equipment->shield, pos);
         sfSprite_setTextureRect(equipment->shield, (sfIntRect)
         {character->pos_animation * 32, 0, 32, 32});
         sfRenderWindow_drawSprite(WINDOW, equipment->shield, NULL);
     }
+}
+
+void update_equipment_inventory_sprite3(project_t *project, box_t *character,
+equipment_t *equipment, sfVector2f pos)
+{
+    if (equipment->ring != NULL) {
+        sfSprite_setPosition(equipment->ring, pos);
+        sfSprite_setTextureRect(equipment->ring, (sfIntRect)
+        {character->pos_animation * 32, 0, 32, 32});
+        sfRenderWindow_drawSprite(WINDOW, equipment->ring, NULL);
+    }
+    if (equipment->sword != NULL) {
+        sfSprite_setPosition(equipment->sword, pos);
+        sfSprite_setTextureRect(equipment->sword, (sfIntRect)
+        {character->pos_animation * 32, 0, 32, 32});
+        sfRenderWindow_drawSprite(WINDOW, equipment->sword, NULL);
+    }
+    update_equipment_inventory_sprite4(project, character, equipment, pos);
 }
 
 void update_equipment_inventory_sprite2(project_t *project, box_t *character,
