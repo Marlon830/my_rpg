95e1590c763189430e440fcae62493bd4620e475
Alexandre Bret <alexandre.bret@epitech.eu>
2023-04-19 11:54:24 +0200

    added ring equipment management

--- src/updates/update_inventory4.c
+++ src/updates/update_inventory4.c
@@ -7,13 +7,24 @@
 
 #include "project.h"
 
-int get_good_path(char *name)
+void update_equipment3_bis(project_t *project, list_box_t *temp, char *path,
+int count)
 {
-    int pos = my_strlen(name) - 1;
-
-    while (name[pos] != '/')
-        pos = pos - 1;
-    return pos + 1;
+    if (count == 5 && temp->box->sprite != NULL)
+        modify_ring(project->player, path);
+    if (count == 5 && temp->box->sprite == NULL &&
+    project->player->equipment->ring != NULL)
+        modify_ring(project->player, NULL);
+    if (count == 6 && temp->box->sprite != NULL)
+        modify_sword(project->player, path);
+    if (count == 6 && temp->box->sprite == NULL &&
+    project->player->equipment->sword != NULL)
+        modify_sword(project->player, NULL);
+    if (count == 7 && temp->box->sprite != NULL)
+        modify_shield(project->player, path);
+    if (count == 7 && temp->box->sprite == NULL &&
+    project->player->equipment->shield != NULL)
+        modify_shield(project->player, NULL);
 }
 
 void update_equipment3(project_t *project, list_box_t *temp, char *path,
@@ -26,16 +37,7 @@ int count)
             my_strcpy(path + my_strlen(path), temp->box->name +
             get_good_path(temp->box->name));
         }
-        if (count == 5 && temp->box->sprite != NULL)
-            modify_sword(project->player, path);
-        if (count == 5 && temp->box->sprite == NULL &&
-        project->player->equipment->sword != NULL)
-            modify_sword(project->player, NULL);
-        if (count == 6 && temp->box->sprite != NULL)
-            modify_shield(project->player, path);
-        if (count == 6 && temp->box->sprite == NULL &&
-        project->player->equipment->shield != NULL)
-            modify_shield(project->player, NULL);
+        update_equipment3_bis(project, temp, path, count);
         count++;
         temp = temp->next;
     }
