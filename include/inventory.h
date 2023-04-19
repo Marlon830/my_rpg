5d1684673efa90f21e3e11d807878c32463452c2
Alexandre Bret <alexandre.bret@epitech.eu>
2023-04-19 14:44:51 +0200

    added get empty box

--- include/inventory.h
+++ include/inventory.h
@@ -84,6 +84,7 @@ box_t *get_box_with_name(list_box_t *list, char *name);
 box_t *get_box_with_coord(project_t *project, sfVector2f pos);
 box_t *get_box_selected(project_t *project);
 box_t *get_box_with_type(inventory_t *inventory, enum object_type type);
+box_t *get_empty_box(inventory_t *inventory);
 sfVector2f convert_mouse_position(project_t *project, sfVector2f pos);
 
 void add_box_to_list(list_box_t *list, box_t *box);
