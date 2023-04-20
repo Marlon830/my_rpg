9f0476d66b95bfb9abd0023f6d186925844fd102
Marlon PEGAHI <marlon.pegahi@epitech.eu>
2023-04-20 19:14:48 +0200

    added a sound when the player discuss with the pnj

--- src/scene/scene_event_bis.c
+++ src/scene/scene_event_bis.c
@@ -15,6 +15,7 @@ void check_all_pnj_dialogue(project_t *project)
     while (temp_pnj != NULL) {
         act_pnj = (all_pnjs_t *) temp_pnj->element;
         if (act_pnj->can_talk) {
+            play_sound(project, "assets/music/dialogue_sound.ogg");
             project->actual_dial->dialogue =
             get_dialogue_by_id(project, project->all_dialogues, act_pnj->dial);
             display_dialogue(project, act_pnj);
