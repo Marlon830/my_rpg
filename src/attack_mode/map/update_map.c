f2fc7dd25c63716a72c81c6896309b2b0dc4dfe8
Alexandre Bret <alexandre.bret@epitech.eu>
2023-04-21 17:18:45 +0200

    added mouse convert when window is resize

--- src/attack_mode/map/update_map.c
+++ src/attack_mode/map/update_map.c
@@ -7,9 +7,11 @@
 
 #include "attack_mode.h"
 
-void update_map(combat_map_t *map, sfEvent event)
+void update_map(sfRenderWindow *window, combat_map_t *map, sfEvent event)
 {
     map->mousePos = (sfVector2f){event.mouseMove.x, event.mouseMove.y};
+    map->mousePos = convert_mouse_window(window, event.mouseMove.x,
+    event.mouseMove.y);
     int b = 0;
     for (int i = 0; i < map->height * map->width; i++) {
         if (is_point_in_diamond(map->mousePos, map->tiles[i])) {
