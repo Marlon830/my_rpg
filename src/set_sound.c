665167ed236f2695aa25d3b427ec351fbdcd9663
Marlon PEGAHI <marlon.pegahi@epitech.eu>
2023-04-21 15:06:10 +0200

    fixed error of not destroying a soundbuffer before reinitizialize it

--- src/set_sound.c
+++ src/set_sound.c
@@ -9,6 +9,8 @@
 
 void play_sound(project_t *project, char *filepath)
 {
+    if (project->soundbuffer != NULL)
+        sfSoundBuffer_destroy(project->soundbuffer);
     project->soundbuffer = sfSoundBuffer_createFromFile(filepath);
     sfSound_setBuffer(project->sound, project->soundbuffer);
     sfSound_stop(project->sound);
