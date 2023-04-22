ede6a833cb5084992b539042947e93e8cfc3ea81
Marlon PEGAHI <marlon.pegahi@epitech.eu>
2023-04-22 15:54:58 +0200

    updated change state with dialogue to add the boat quest

--- src/states/changes_dialogue.c
+++ src/states/changes_dialogue.c
@@ -19,6 +19,7 @@ void change_state_with_dialogue(project_t *project, all_pnjs_t *act_pnj)
     client_quest(project, act_pnj);
     dungeon_door1_quest(project, act_pnj);
     squest_quest(project, act_pnj);
+    boat_quest(project, act_pnj);
 }
 
 void change_state_with_scene(project_t *project, int to_scene_id)
@@ -37,6 +38,10 @@ void set_all_pnj_dialogues_ter(project_t *project)
     }
     if (project->player->player_second_state >= 3) {
         set_pnj_dialogue(project->scene->pnj, "/enemy_sq1.png", "Squest3");
+        set_pnj_dialogue(project->scene->pnj, "/boat_guy.png", "Boat2");
+    }
+    if (project->player->player_second_state >= 4) {
+        set_pnj_dialogue(project->scene->pnj, "/boat_guy.png", "Boat3");
     }
 }
 
@@ -77,6 +82,7 @@ void set_all_pnj_dialogues(project_t *project)
         set_pnj_dialogue(project->scene->pnj, "/client.png", "Client1");
         set_pnj_dialogue(project->scene->pnj, "/door_boss1.png", "DoorBoss1");
         set_pnj_dialogue(project->scene->pnj, "/enemy_sq1.png", "Squest1");
+        set_pnj_dialogue(project->scene->pnj, "/boat_guy.png", "Boat1");
     }
     if (project->player->player_progress_state >= 1) {
         set_pnj_dialogue(project->scene->pnj, "/theodore.png", "Theoronfle2");
