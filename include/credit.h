98d3c43fd70c4facbb5dbbfe8c45691a31ac0ca2
Alexandre Bret <alexandre.bret@epitech.eu>
2023-04-22 15:24:27 +0200

    added epic music for credits

--- include/credit.h
+++ include/credit.h
@@ -21,10 +21,13 @@ typedef struct credit_s {
     sfText *text;
     sfFont *font;
     sfVector2f pos;
+    sfMusic *music;
+    float volume;
 } credit_t;
 
 credit_t *create_credit(void);
 void update_credit(project_t *project, credit_t *credit);
 void credit_event(project_t *project);
+void start_credit(project_t *project);
 
 #endif
